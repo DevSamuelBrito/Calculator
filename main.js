dcee3eaa9fea782f2fba7c713fd92c6351354723
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-04-05 09:31:59 -0300

    creating calculator functions

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-const result = document.querySelector(".result");
+const result = document.getElementById("result");
+
+function appendToResult(input) {
+  result.value += input;
+}
+
+function clearResult() {
+  result.value = ``;
+}
+
+function calculate() {
+  try {
+    result.value = eval(result.value);
+  } catch (error) {
+    result.value = "Error";
+  }
+}
+
+function deleteResult() {
+  let valorInput = result.value;
+
+  // Verificar se há caracteres para apagar
+  if (valorInput.length > 0) {
+    // Apagar o último caractere
+    valorInput = valorInput.slice(0, -1);
+    // Atualizar o valor do input
+    result.value = valorInput;
+  }
+}
